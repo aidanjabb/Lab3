72ef34e1c8ab0e4dc662d6fd6c6c291315c870ef
aidan_jabbarzadeh <jabbarz2@illinois.edu>
2018-02-06 16:54:12 -0600

    initial commit

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,5 +1,42 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+
 public class WebScraper {
-    public static void main(String[] unused) {
+
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
 
     }
+    public static void main(String[] unused) {
+        System.out.println(urlToString("http://erdani.com/tdpl/hamlet.txt").length());
+        System.out.println(urlToString("https://www.bls.gov/tus/charts/chart9.txt").length());
+        System.out.println(urlToString("http://tgftp.nws.noaa.gov/data/raw/fz/fzus53.klot.srf.lot.txt").length());
+
+        int princeCount = 0;
+        String urlString = urlToString("http://erdani.com/tdpl/hamlet.txt");
+        for (int i = 0; i < urlString.length(); i++) {
+            if (urlString.contains("Prince")
+                    || urlString.contains("prince")) {
+                princeCount++;
+            }
+        }
+        System.out.println(princeCount);
+    }
+
 }
